09d6dd59e74ef3d68716e1ea651ba1fe8efcffdb
sushil-arya <sushilarya888@gmail.com>
2025-01-20 21:47:14 +0530

    project3 using object insted of primitive values and in place of action

--- src/components/CounterObject/CounterObject.jsx
+++ src/components/CounterObject/CounterObject.jsx
@@ -1,14 +1,22 @@
 import {React,useReducer} from 'react'
-const initialState = 0;
+const initialState={
+  counter : 0,
+  counter1: 5
+}
 function reducer(state,action){
-  switch(action){
+  switch(action.type){
     case 'increment':
-      return state+1;
+      return {...state,counter: state.counter+action.payload};
     case 'decrement':
-      if(state>0)
-      return state-1;
+      if(state.counter>0)
+        return {...state,counter: state.counter-action.payload};
+    case 'increment1':
+        return {...state,counter1: state.counter1+action.payload};
+    case 'decrement1':
+      if(state.counter1>5)
+        return {...state,counter1: state.counter1-action.payload};
     case'reset':
-      return 0;
+      return initialState;
     default:
       return state;
   }
@@ -24,11 +32,25 @@ const CounterObject = () => {
 
   return (
     <div className="counterContainer">
-      <h1>Count - {count}</h1>
+      <h1>Count - {count.counter}</h1>
+      <h1>Count - {count.counter1}</h1>
       <div className="btn-container">
-        <button onClick={()=>dispatch('increment')} className="increment-btn btn">Increment</button>
-        <button onClick={()=>dispatch('decrement')} className="decrement-btn btn">Decrement</button>
-        <button onClick={()=>dispatch('reset')} className="reset-btn btn">Reset</button>
+        <button onClick={()=>dispatch({type:'increment', payload:1})} className="increment-btn btn">Increment</button>
+        <button onClick={()=>dispatch({type:'decrement', payload:1})} className="decrement-btn btn">Decrement</button>
+        
+        {/* increment and decrement by 5 */}
+        <button onClick={()=>dispatch({type:'increment', payload:5})} className="increment-btn btn">Increment</button>
+        <button onClick={()=>dispatch({type:'decrement', payload:5})} className="decrement-btn btn">Decrement</button>
+
+
+        {/* increment and decrement by 2*/}
+        <button onClick={()=>dispatch({type:'increment1', payload:2})} className="increment-btn btn">Increment</button>
+        <button onClick={()=>dispatch({type:'decrement1', payload:2})} className="decrement-btn btn">Decrement</button>
+
+
+        <button onClick={()=>dispatch({type:'reset'})} className="reset-btn btn">Reset</button>
+
+      
       </div>
     </div>
   )
